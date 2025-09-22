59b963ffe918d76de4ecbe8030362513a5be04df
William Gonzalez <willfgz@gmail.com>
2025-09-21 22:56:45 -0300

    Criação de tela de Relatórios

---
+++ src/main/java/com/projeto/gestao/controller/RelatoriosController.java
@@ -0,0 +1,15 @@
+package com.projeto.gestao.controller;
+
+import javafx.scene.control.Label;
+
+public class RelatoriosController extends BaseController {
+    public Label lblUsuarios;
+    public Label lblEquipes;
+    public Label lblProjetos;
+
+
+    @Override
+    protected void configurarTela() {
+
+    }
+}
